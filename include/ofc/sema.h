2c6814df4d6bd2e03657eedfe8adc2059375d4b6
Beleth Apophis <beleth.apophis@codethink.co.uk>
2017-10-19 17:28:39 +0100

    MOVE TO AST: enums: accessibility, kind

--- include/ofc/sema.h
+++ include/ofc/sema.h
@@ -38,7 +38,9 @@ typedef struct ofc_sema_lhs_list_s          ofc_sema_lhs_list_t;
 typedef struct ofc_sema_module_list_s       ofc_sema_module_list_t;
 typedef struct ofc_sema_format_label_list_s ofc_sema_format_label_list_t;
 
-#include <ofc/sema/kind.h>
+#include <ast/sema.h>
+
+//#include <ofc/sema/kind.h>
 #include <ofc/sema/array.h>
 #include <ofc/sema/structure.h>
 #include <ofc/sema/typeval.h>
@@ -53,7 +55,7 @@ typedef struct ofc_sema_format_label_list_s ofc_sema_format_label_list_t;
 #include <ofc/sema/io.h>
 #include <ofc/sema/arg.h>
 #include <ofc/sema/range.h>
-#include <ofc/sema/accessibility.h>
+//#include <ofc/sema/accessibility.h>
 
 #include <ofc/sema/stmt.h>
 #include <ofc/sema/type.h>
