98a74f5b769afa66d00e80a349259395ca130297
Beleth Apophis <beleth.apophis@codethink.co.uk>
2017-10-19 15:42:34 +0100

    MOVE TO AST: string

--- include/ast/string.h
+++ include/ast/string.h
@@ -13,22 +13,15 @@
  * limitations under the License.
  */
 
-#ifndef __ofc_string_h__
-#define __ofc_string_h__
+#ifndef __ast_string_h__
+#define __ast_string_h__
 
 #include <stdbool.h>
-#include <ast/string.h>
 
-ofc_string_t* ofc_string_create(const char* base, unsigned size);
-ofc_string_t* ofc_string_copy(const ofc_string_t* src);
-void          ofc_string_delete(ofc_string_t* string);
-
-bool ofc_string_empty(const ofc_string_t* string);
-
-/* May return truncated string if string contains nul characters. */
-const char* ofc_string_strz(const ofc_string_t* string);
-
-unsigned ofc_string_length(const ofc_string_t* string);
-bool     ofc_string_equal(const ofc_string_t a, const ofc_string_t b);
+typedef struct
+{
+	char*    base;
+	unsigned size;
+} ofc_string_t;
 
 #endif
